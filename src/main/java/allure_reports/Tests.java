2f76685348038d1ccd61984136aa6e3cf9bee6be
Kutsolabskiy <kutsolabskiytattoo@gmail.com>
2021-05-27 20:37:14 +0300

    asd

--- src/main/java/allure_reports/Tests.java
+++ src/main/java/allure_reports/Tests.java
@@ -35,7 +35,7 @@ public class Tests{
         Assert.assertTrue(isStatus);
     }
 
-    @Test(priority = 2)
+ //   @Test(priority = 2)
     @Description("Verify Login........")
     @Epic("EP001")
     @Feature("Feature 1: Login")
@@ -69,7 +69,7 @@ public class Tests{
         Assert.assertEquals(driver.getTitle(), "Success log in");
     }
 
-    @Test(priority = 3)
+  //  @Test(priority = 3)
     @Description("Verify Login........")
     @Epic("EP001")
     @Feature("Feature 1: Login")
