0157e8fb2e3902d497f7f10c7064e0c46f4b1451
Kutsolabskiy <kutsolabskiytattoo@gmail.com>
2021-05-27 14:10:55 +0300

    asd

---
+++ src/main/java/allure_reports/Main.java
@@ -0,0 +1,13 @@
+package allure_reports;
+
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
